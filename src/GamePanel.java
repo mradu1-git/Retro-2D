3436adb0e5cd5dbade6f7219fd463a6fde54126a
Mradul Pal <mradulpal@outlook.com>
2026-06-01 03:00:46 +0530

    Player Introduced in JPanel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -10,6 +10,11 @@ public class GamePanel extends JPanel implements ActionListener {
         this.setPreferredSize(new Dimension(800, 600));
         this.setBackground(Color.BLACK);
 
+        this.input = new KeyboardHandler();
+        this.player = new PlayerCar(new Vector2D(400, 300));
+        this.setFocusable(true);
+        this.addKeyListener(this.input);
+
         int FPS = 60;
         int DELAY = 1000 / FPS;
         this.gameTimer = new Timer(DELAY, (ActionListener) this);
@@ -33,6 +38,24 @@ public class GamePanel extends JPanel implements ActionListener {
         g2d.fillRect(375, 450, 50, 80);
 
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        double carX = player.getPosition().getX();
+        double carY = player.getPosition().getY();
+        double carAngle = player.getAngle();
 
+        long standardTransform = (long) g2d.getTransform().hashCode();
+        g2d.translate(carX, carY);
+        g2d.rotate(carAngle);
+
+        int width = 40;
+        int height = 70;
+
+        g2d.setColor(Color.RED);
+        g2d.fillRect( -width / 2, -height / 2, width, height);
+
+        g2d.setColor(Color.YELLOW);
+        g2d.fillRect(-15, -height / 2 - 5, 10, 5);
+        g2d.fillRect(5, -height / 2 - 5, 10, 5);
+
+        g2d.setTransform(new java.awt.geom.AffineTransform());
     }
 }
